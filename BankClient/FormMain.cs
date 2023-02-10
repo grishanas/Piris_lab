a0d4e55da0bda2cfb967821486ab00e70d10422a
Crumbler <25861574+Crumbler@users.noreply.github.com>
2023-02-10 12:38:16 +0300

    Create BankClient project

---
+++ BankClient/FormMain.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace BankClient
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
